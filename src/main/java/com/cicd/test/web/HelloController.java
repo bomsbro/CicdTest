c344d4e5e2e87d6fb24350d3732eac78b69adc2e
bomsbro <transcend716@naver.com>
2021-09-30 17:19:38 +0900

    init: init project

---
+++ src/main/java/com/cicd/test/web/HelloController.java
@@ -0,0 +1,15 @@
+package com.cicd.test.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+}
